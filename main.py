2c9a84e77b9251b1cd6aba8a575d10e06b4d8a82
julian1c2a <julian.calderon.almendros@gmail.com>
2025-02-19 14:39:35 +0100

    Generación de listas de números de Bernoulli consecutivos

---
+++ main.py
@@ -0,0 +1,74 @@
+from decimal import Decimal as Double, getcontext
+from fractions import Fraction as Fract
+
+
+def calc_binom(n: int, k: int) -> int:
+    if (n < 0) or (k < 0) or (k > n):
+        return 0
+    elif n == k or k == 0:
+        return 1
+    elif n - 1 == k or k == 1:
+        return n
+    elif n - 2 == k or k == 2:
+        return n * (n - 1) // 2
+    elif n - 3 == k or k == 3:
+        return n * (n - 1) * (n - 2) // 6
+    elif n - 4 == k or k == 4:
+        return n * (n - 1) * (n - 2) * (n - 3) // 24
+    else:
+        # result = 1
+        # for i in range(1,k+1):
+        #    result *= (n+1-i)//(i*(n-k-i))
+        # return result
+        return (n * calc_binom ( n - 1, k - 1 )) // k
+
+
+def calc_bernoulli_list(n: int) -> list[Fract]:
+    lista_bernoulli: list[Fract] = [Fract ( 0, 1 )] * (n + 1)
+    for k in range ( 0, n + 1 ):
+        if k == 0:
+            lista_bernoulli[0] = Fract ( 1, 1 )
+        elif k == 1:
+            lista_bernoulli[1] = Fract ( -1, 2 )
+        elif k % 2 == 1:
+            lista_bernoulli[k] = Fract ( 0, 1 )
+        else:
+            result: Fract = Fract ( 0, 1 )
+            for l in range ( 0, k ):
+                result += Fract ( calc_binom ( k + 1, l ), l + 1 ) * lista_bernoulli[l]
+            lista_bernoulli[k] = -result
+    return lista_bernoulli
+
+
+if __name__ == '__main__':
+    # N : int = 20
+    # for l in range(0,N+1):
+    #    print(f'Coeficientes binomiales de {l}')
+    #    suma = 0
+    #    for i in range(0,l+1):
+    #        binnumber = calc_binom( l, i )
+    #        print(f"Binomial({l},{i})  =  {binnumber}")
+    #        suma += binnumber
+    #    print(f"Sum_(k=0)^({l})[Binomial({l},k)]  = {suma} y 2^{l} = {2**l}")
+    #    print("\n")
+
+    print ( " --------------------------------------------------------- " )
+    print ( " -------------- NUMEROS DE BERNOULLI EXACTOS ------------- " )
+    print ( " --------------------------------------------------------- " )
+
+    lista_de_numeros_de_Bernoulli = calc_bernoulli_list ( 100 )
+    for ix in range ( 0, len ( lista_de_numeros_de_Bernoulli ) ):
+        if lista_de_numeros_de_Bernoulli[ix] != Fract ( 0, 1 ):
+            print ( f"Bernoulli[{ix}]  =  {lista_de_numeros_de_Bernoulli[ix]}" )
+
+    print ( " --------------------------------------------------------- " )
+    print ( " ------------ NUMEROS DE BERNOULLI EN COMA FLOTANTE ------ " )
+    print ( " --------------------------------------------------------- " )
+
+    getcontext ().prec = 120
+
+    for ix in range ( 0, len ( lista_de_numeros_de_Bernoulli ) ):
+        if lista_de_numeros_de_Bernoulli[ix] != Fract ( 0, 1 ):
+            numerador: Double = Double ( lista_de_numeros_de_Bernoulli[ix].numerator )
+            denominador: Double = Double ( lista_de_numeros_de_Bernoulli[ix].denominator )
+            print ( f"Bernoulli[{ix}]  =  {numerador / denominador}" )
